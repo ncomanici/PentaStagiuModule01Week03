d5e51d4181a5eaf7ddea4add76a452c16038a672
ncomanici <ncomanici@pentalog.com>
2018-05-16 14:34:40 +0300

    initial commit

---
+++ UniversityLibrary/Teacher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityLibrary
+{
+    public class Teacher : Person
+    {
+        // default value is null
+        public string ScientificTitle { get; private set; }
+
+        public Teacher(string scientificTitle, string firstName, string lastName, DateTime birthDate, Gender gender)
+            : base(firstName, lastName, birthDate, gender)
+        {
+            this.ScientificTitle = scientificTitle;
+        }
+    }
+}
---
+++ UniversityLibrary/University.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityLibrary
+{
+    public class University
+    {
+        private List<Student> students;
+        private List<Teacher> teachers;
+
+        public University()
+        {
+            this.students = new List<Student>();
+            this.teachers = new List<Teacher>();
+        }
+    }
+}
